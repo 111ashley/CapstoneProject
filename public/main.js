886b29f74d4cb2c4f92d3a01b4f56cb969684dbb
Ashley Ahamed <ahamedashley7@gmail.com>
2022-08-05 20:56:44 -0400

    Added containers and code for buttons

--- public/main.js
+++ public/main.js
@@ -4,6 +4,10 @@ const errCallback = err => console.log(err)
 const form = document.querySelector('form')
 const itemsContainer = document.querySelector('#items-container')
 const browseBtn = document.querySelector('#see-all')
+const choicesDiv = document.querySelector('#choices')
+const userListDiv = document.querySelector('#user-list')
+const browseHeader = document.querySelector('#browse-header')
+const yourListHeader = document.querySelector('#your-list-header')
 
 
 const getItems = () => {
@@ -16,7 +20,11 @@ displayItems(res.data);
 .catch((err) => console.log(err));
  };
 
- let choices = []
+let choices = []
+let userList = []
+
+yourListHeader.classList.add('hide')
+
 
 function createItemCard(item){
     console.log(item)
@@ -30,12 +38,56 @@ function createItemCard(item){
     itemsContainer.appendChild(itemCard)
 }
 
+function createUserItemCard(item){
+    const itemCard = document.createElement('div')
+    itemCard.classList.add('item-card')
+    itemCard.innerHTML = `<img alt='${item.name}' src=${item.imgURL} class="item-image"/>
+    <p class="name" style="color:#fff;">${item.name}</p> 
+    <p class="price" style="color:#fff;">${item.price}</p>
+    <button class="item-btn" style="background-color:#83D0CB; border: solid 2px #000" onclick="putItemBack(${item.id}")">REMOVE FROM LIST</button>
+    `
+    itemsContainer.appendChild(itemCard)
+}
+
 function displayItems(arr) {
     itemsContainer.innerHTML = ``
     for (let i = 0; i < arr.length; i++){
         createItemCard(arr[i])
     }
 }
+
+const changeItems = () => {
+    choicesDiv.innerHTML = ''
+    browseHeader.classList.remove('hide')
+
+    choices.forEach(choice => {
+        let itemHtml = createItemCard(choice)
+        choicesDiv.innerHTML += itemHtml
+    })
+}
+
+const changeItemsList = () => {
+    choicesDiv.innerHTML = ''
+    browseHeader.classList.remove('hide')
+
+    choices.forEach(choice => {
+        let itemHtml = createItemCard(choice)
+        choicesDiv.innerHTML += itemHtml
+    })
+}
+
+const putItemBack = (id) => {
+    let index = userList.findIndex(item => item.id === id)
+    choices.push(userList[index])
+    userList.splice(index, 1)
+    changeItems()
+    changeItemsList()
+    // duelBtn.classList.add('hide')
+    if (userList.length === 0) {
+        yourListHeader.classList.add('hide')
+    }
+}
+
 
 getItems()
 
